5569ffb119729d994b44f8a523e573c403fd0ed8
Jack Roberts <roberts@pnut.com>
2015-11-09 14:45:15 -0500

    Initial checkin of code to implement RA example.

---
+++ global.R
@@ -0,0 +1,51 @@
+
+library(shinyapps)
+library(googleVis)
+library(knitr)
+library(shiny)
+# install.packages("tm")
+#library(tm)
+# install.packages("wordcloud")
+#library(wordcloud)
+#library(memoise)
+
+# devtools::install_github("AnalytixWare/ShinySky")
+library(shinysky)
+
+#download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
+load("mlb11.RData")
+
+rownames(mlb11) <- mlb11$team
+mlb11$team <- NULL
+myTheme <- "bootstrap-cosmo.css"
+myName <- "Company Name"
+
+# The list of valid books
+# books <<- list("A Mid Summer Night's Dream" = "summer",
+#                "The Merchant of Venice" = "merchant",
+#                "Romeo and Juliet" = "romeo")
+# 
+# # Using "memoise" to automatically cache the results
+# getTermMatrix <- memoise(function(book) {
+#   # Careful not to let just any name slip in here; a
+#   # malicious user could manipulate this value.
+#   if (!(book %in% books))
+#     stop("Unknown book")
+#   
+#   text <- readLines(sprintf("./%s.txt.gz", book),
+#                     encoding="UTF-8")
+#   
+#   myCorpus = Corpus(VectorSource(text))
+#   myCorpus = tm_map(myCorpus, content_transformer(tolower))
+#   myCorpus = tm_map(myCorpus, removePunctuation)
+#   myCorpus = tm_map(myCorpus, removeNumbers)
+#   myCorpus = tm_map(myCorpus, removeWords,
+#                     c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
+#   
+#   myDTM = TermDocumentMatrix(myCorpus,
+#                              control = list(minWordLength = 1))
+#   
+#   m = as.matrix(myDTM)
+#   
+#   sort(rowSums(m), decreasing = TRUE)
+# })
